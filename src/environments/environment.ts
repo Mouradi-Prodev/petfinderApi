b3f8360a2955f7be1e520ec86029a1eb0330dda2
unknown <sallah.mouradi19@gmail.com>
2024-04-23 02:26:02 +0200

    Deleted a secret key exposed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
       projectId: "petfinder-60f88",
       appId: "1:1042131774621:web:ac93a25bf16e2ef00eb874",
       storageBucket: "petfinder-60f88.appspot.com",
-      apiKey: "AIzaSyAE1VW2QgQsOLr8fPxsinn9MrTHXIG8ptk",
+      apiKey: "secret",
       authDomain: "petfinder-60f88.firebaseapp.com",
       messagingSenderId: "1042131774621"
     },
